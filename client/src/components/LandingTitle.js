77e8d771da18ee423d730ea79c33760e0d5bb8b0
Techcam1 <167629538+Techcam1@users.noreply.github.com>
2024-08-10 16:56:17 -0400

    adding updates

---
+++ client/src/components/LandingTitle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="App-header"> 
+      <h1 className="header-text">LET US <br></br> DO THE WORK.</h1>
+     
+     
+    </header>
+  );
+}
+
+export default Header;
+
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import OurStory from '../components/OurStory'
+import React from 'react';
+import '../App.css';
+import { Link } from 'react-router-dom';
 
-const Landing = () => {
+
+import Title from '../components/LandingTitle';
+import Footer from '../components/Footer';
+
+function Landing() {
+  const handleButtonClick = (message) => {
+    alert(message);
+  };
+
   return (
-    <OurStory/>
-
-  )
+    <div className="App">
+   
+      <div className="split-background">
+        <div className="left-side">
+          <Title />
+        </div>
+        <div className="right-side">
+          <div className="links">
+            <Link to="/ourstory">OUR STORY</Link> 
+            <Link to="/recipepage">OUR RECIPES</Link>
+            {/* <a href="#story">OUR STORY</a> */}
+            {/* <a href="#recipes">OUR RECIPES</a> */}
+            
+          </div>
+          <h1>EXPLORE OUR RECIPES</h1>
+          <a>... but first</a>
+          <img src="fork.png" alt="Fork" className="fork-image" />
+          <button onClick={() => handleButtonClick('Sign in button clicked')}>
+            Sign In
+          </button>
+        </div>
+      </div>
+      
+     
+      <Footer />
+    </div>
+  );
 }
-
 export default Landing
